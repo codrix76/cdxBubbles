7e44f316d9d5c48462a1946b57d08c1061f3d147
codrix76 <tim.parmax@gmx.de>
2025-05-31 11:09:34 +0200

    restart

--- cdxBubbleComponents/Views/BubbleElement.xaml.cs
+++ cdxBubbleComponents/Views/BubbleElement.xaml.cs
@@ -1,5 +1,4 @@
-﻿
-using cdxBubbleComponents.Models;
+﻿using cdxBubbleComponents.ViewModels;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,30 +12,7 @@ namespace cdxBubbleComponents.Views
         public BubbleElement()
         {
             InitializeComponent();
-#if DEBUG
-            if (DesignerProperties.GetIsInDesignMode(this))
-            {
-                // Design-Daten für Vorschau
-                this.DataContext = new BubbleElementModel
-                {
-                    Label = "Demo",
-                    IsActive = true,
-                    IsHighlighted = false,
-                    Style = new BubbleVisualStyle
-                    {
-                        Background = Brushes.DarkSlateBlue,   // ✅ Brush statt Color
-                        Foreground = Brushes.White,
-                        BorderThickness = new Thickness(1),
-                        CornerRadiusCircular = new CornerRadius(20),
-                        CornerRadiusTextual = new CornerRadius(12),
-                        SizeFactor = 1.0,
-                        FontSize = 16,
-                        Transparency = 1.0
-                    }
-                };
-
-            }
-#endif
+            this.DataContext = new BubbleElementModel();
         }
     }
 }
