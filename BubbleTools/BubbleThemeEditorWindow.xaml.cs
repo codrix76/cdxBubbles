6f242508509f2f25d1bb4fb24ccce885a2fd4554
codrix76 <tim.parmax@gmx.de>
2025-06-19 20:33:27 +0200

    new Tool for creating Themes

---
+++ BubbleTools/BubbleThemeEditorWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using BubbleControlls.ControlViews;
+using BubbleControlls.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace BubbleTools
+{
+    /// <summary>
+    /// Interaktionslogik für BubbleThemeEditorWindow.xaml
+    /// </summary>
+    public partial class BubbleThemeEditorWindow : Window
+    {
+        private BubbleVisualTheme _currentTheme = BubbleVisualThemes.Standard();
+        private BubbleMenu PreviewBubble = new BubbleMenu();
+        public BubbleThemeEditorWindow()
+        {
+            InitializeComponent();
+            UpdatePreview(_currentTheme);
+            ThemeEditorBuilder.Build(ConfigPanel, _currentTheme, () =>
+            {
+                UpdatePreview(_currentTheme);
+                PreviewBubble.InvalidateVisual();
+            });
+            DisplayButton.Click += DisplayButton_Click;
+            SaveButton.Click += SaveButton_Click;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "theme.json");
+            ExportToFile(_currentTheme, path);
+        }
+
+        private void DisplayButton_Click(object sender, RoutedEventArgs e)
+        {
+            string code = GenerateCode(_currentTheme, "MyGeneratedTheme");
+            Window codeWindow = CreateCodeWindow(code);
+            codeWindow.Show();
+        }
+
+        private void UpdatePreview(BubbleVisualTheme currentTheme)
+        {
+            PreviewPanel.Children.Clear();
+            PreviewBubble = new BubbleMenu();
+            PreviewBubble.Width = 200;
+            PreviewBubble.Height = 200;
+            PreviewBubble.VerticalAlignment = VerticalAlignment.Top;
+            PreviewBubble.HorizontalAlignment = HorizontalAlignment.Left;
+            PreviewBubble.MenuStyleTheme = currentTheme;
+
+            PreviewPanel.Children.Add(PreviewBubble);
+        }
+
+        private Window CreateCodeWindow(string code)
+        {
+            var window = new Window
+            {
+                Title = "Exportierter Theme-Code",
+                Width = 800,
+                Height = 600,
+                Content = new TextBox
+                {
+                    Text = code,
+                    FontFamily = new FontFamily("Consolas"),
+                    FontSize = 13,
+                    IsReadOnly = true,
+                    AcceptsReturn = true,
+                    AcceptsTab = true,
+                    TextWrapping = TextWrapping.Wrap,
+                    VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+                    HorizontalScrollBarVisibility = ScrollBarVisibility.Auto,
+                    Margin = new Thickness(10)
+                }
+            };
+            return window;
+        }
+        private string GenerateCode(BubbleVisualTheme theme, string methodName = "MyTheme")
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"public static BubbleVisualTheme {methodName}()");
+            sb.AppendLine("{");
+            sb.AppendLine("    return new BubbleVisualTheme");
+            sb.AppendLine("    {");
+
+            var props = typeof(BubbleVisualTheme).GetProperties();
+
+            foreach (var prop in props)
+            {
+                var val = prop.GetValue(theme);
+
+                if (val is SolidColorBrush brush)
+                {
+                    var c = brush.Color;
+                    sb.AppendLine($"        {prop.Name} = new SolidColorBrush(Color.FromArgb({c.A}, {c.R}, {c.G}, {c.B})),");
+                }
+                else if (val is Color color)
+                {
+                    sb.AppendLine($"        {prop.Name} = Color.FromArgb({color.A}, {color.R}, {color.G}, {color.B}),");
+                }
+                else if (val is FontFamily font)
+                {
+                    sb.AppendLine($"        {prop.Name} = new FontFamily(\"{font.Source}\"),");
+                }
+                else if (val is FontWeight weight)
+                {
+                    sb.AppendLine($"        {prop.Name} = FontWeights.{weight.ToString()},");
+                }
+                else if (val is FontStyle style)
+                {
+                    sb.AppendLine($"        {prop.Name} = FontStyles.{style.ToString()},");
+                }
+                else if (val is Thickness t)
+                {
+                    sb.AppendLine($"        {prop.Name} = new Thickness({t.Left}),");
+                }
+                else if (val is bool b)
+                {
+                    sb.AppendLine($"        {prop.Name} = {b.ToString().ToLower()},");
+                }
+                else if (val is int i)
+                {
+                    sb.AppendLine($"        {prop.Name} = {i},");
+                }
+                else if (val is double d)
+                {
+                    sb.AppendLine($"        {prop.Name} = {d.ToString(System.Globalization.CultureInfo.InvariantCulture)},");
+                }
+            }
+
+            sb.AppendLine("    };");
+            sb.AppendLine("}");
+
+            return sb.ToString();
+        }
+
+        private void ExportToFile(BubbleVisualTheme theme, string filePath)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Auto
+            };
+
+            string json = JsonConvert.SerializeObject(theme, settings);
+            File.WriteAllText(filePath, json);
+        }
+
+        private string ExportToString(BubbleVisualTheme theme)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                TypeNameHandling = TypeNameHandling.Auto
+            };
+
+            return JsonConvert.SerializeObject(theme, settings);
+        }
+
+    }
+}
---
+++ BubbleTools/ThemeEditorBuilder.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using BubbleControlls.Models;
+using Xceed.Wpf.Toolkit;
+using System.ComponentModel.DataAnnotations;
+
+namespace BubbleTools
+{
+    public static class ThemeEditorBuilder
+    {
+        public static void Build(StackPanel panel, BubbleVisualTheme theme, Action onChanged)
+        {
+            panel.Children.Clear();
+
+            var props = typeof(BubbleVisualTheme).GetProperties();
+
+            foreach (var prop in props)
+            {
+                var display = prop.GetCustomAttribute<DisplayAttribute>();
+                string name = display?.Name ?? prop.Name;
+                string desc = display?.Description ?? "";
+
+                // Container mit Label
+                var container = new StackPanel { Margin = new Thickness(0, 5, 0, 5) };
+                var label = new TextBlock
+                {
+                    Text = name,
+                    ToolTip = string.IsNullOrWhiteSpace(desc) ? null : desc,
+                    FontWeight = FontWeights.SemiBold
+                };
+                container.Children.Add(label);
+
+                FrameworkElement? editor = CreateEditor(prop, theme, onChanged);
+                if (editor != null)
+                    container.Children.Add(editor);
+
+                panel.Children.Add(container);
+            }
+        }
+
+        private static FrameworkElement? CreateEditor(PropertyInfo prop, BubbleVisualTheme theme, Action onChanged)
+        {
+            var type = prop.PropertyType;
+
+            // COLOR
+            if (type == typeof(Color))
+            {
+                var picker = new ColorPicker
+                {
+                    UsingAlphaChannel = false,
+                    SelectedColor = (Color?)prop.GetValue(theme)
+                };
+                picker.SelectedColorChanged += (_, e) =>
+                {
+                    if (e.NewValue is Color c)
+                    {
+                        prop.SetValue(theme, c);
+                        onChanged();
+                    }
+                };
+                return picker;
+            }
+
+            // BRUSH (SolidColorBrush)
+            if (type == typeof(Brush))
+            {
+                var brush = prop.GetValue(theme) as SolidColorBrush;
+                var picker = new ColorPicker
+                {
+                    UsingAlphaChannel = true,
+                    SelectedColor = brush?.Color
+                };
+                picker.SelectedColorChanged += (_, e) =>
+                {
+                    if (e.NewValue is Color c)
+                    {
+                        prop.SetValue(theme, new SolidColorBrush(c));
+                        onChanged();
+                    }
+                };
+                return picker;
+            }
+
+            // DOUBLE
+            if (type == typeof(double))
+            {
+                var slider = new Slider
+                {
+                    Minimum = 0,
+                    Maximum = 100,
+                    Width = 200,
+                    Value = (double)prop.GetValue(theme)
+                };
+                slider.ValueChanged += (_, _) =>
+                {
+                    prop.SetValue(theme, slider.Value);
+                    onChanged();
+                };
+                return slider;
+            }
+
+            // INT
+            if (type == typeof(int))
+            {
+                var slider = new Slider
+                {
+                    Minimum = 0,
+                    Maximum = 255,
+                    Width = 200,
+                    Value = (int)prop.GetValue(theme)
+                };
+                slider.ValueChanged += (_, _) =>
+                {
+                    prop.SetValue(theme, (int)slider.Value);
+                    onChanged();
+                };
+                return slider;
+            }
+
+            // BOOL
+            if (type == typeof(bool))
+            {
+                var box = new CheckBox
+                {
+                    IsChecked = (bool)prop.GetValue(theme),
+                };
+                box.Checked += (_, _) => { prop.SetValue(theme, true); onChanged(); };
+                box.Unchecked += (_, _) => { prop.SetValue(theme, false); onChanged(); };
+                return box;
+            }
+
+            // FONT FAMILY (TextBox)
+            if (type == typeof(FontFamily))
+            {
+                var box = new TextBox
+                {
+                    Text = ((FontFamily?)prop.GetValue(theme))?.Source ?? "",
+                    Width = 200
+                };
+                box.LostFocus += (_, _) =>
+                {
+                    prop.SetValue(theme, new FontFamily(box.Text));
+                    onChanged();
+                };
+                return box;
+            }
+
+            // FONT WEIGHT (TextBox einfach)
+            if (type == typeof(FontWeight))
+            {
+                var box = new TextBox
+                {
+                    Text = ((FontWeight)prop.GetValue(theme)).ToString(),
+                    Width = 200
+                };
+                box.LostFocus += (_, _) =>
+                {
+                    if (Enum.TryParse<FontWeight>(box.Text, true, out var weight))
+                    {
+                        prop.SetValue(theme, weight);
+                        onChanged();
+                    }
+                };
+                return box;
+            }
+
+            return null;
+        }
+    }
+}
